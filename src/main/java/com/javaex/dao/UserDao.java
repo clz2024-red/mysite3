380704c1d525f6bff0dd5d3179b11505c4a4f89d
clz2024.red <clz2024.red@gmail.com>
2024-02-21 16:06:28 +0900

    2024-02-21

---
+++ src/main/java/com/javaex/dao/UserDao.java
@@ -0,0 +1,69 @@
+package com.javaex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.javaex.vo.UserVo;
+
+public class UserDao {
+
+	// 필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/web_db";
+	private String id = "web";
+	private String pw = "web";
+
+	// 연결
+	public void getConnection() {
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+
+		}
+	}
+
+	// 종료
+	public void close() {
+		// 5. 자원정리
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+
+	public int insertUser(UserVo userVo) {
+		
+		this.getConnection();
+		
+		//insert코드
+		
+		this.close();
+		
+		return 1;
+	}
+
+}
